50c639725c5c4208fa069a496037378eab3aff2d
Karen Flores <karen.ag.flores@gmail.com>
2018-04-11 08:32:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,8 +80,8 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, item|
       holidays = holiday.to_s.capitalize
       if holidays.include?("_")
-        holiday1s = holidays.gsub("_", " ")
-        holidays = holiday1s.to_s.humanize
+        holiday1s = holidays.gsub("_", " ").to_s
+        holidays = holiday1s.capitalize
       end
       #supplies = holiday_hash[season][holiday]
       #supplies2 = supplies.values
